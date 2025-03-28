bbf88becad9f1c248e223b28bbaa3038873bb50d
TasawarSaraf <tazsaraf@gmail.com>
2025-03-27 23:33:28 -0400

    Initial Commit

---
+++ FloatThoughts/FloatThoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatThoughtsApp.swift
+//  FloatThoughts
+//
+//  Created by Tasawar Saraf on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FloatThoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
